003b9e9ee628a60e151c76e0542c4a1bcf0bfdc7
WNomunomu <kohsei.nmr@gmail.com>
2025-07-01 16:44:41 +0000

    implement MyApplication

--- src/main/java/com/example/MyApplication.kt
+++ src/main/java/com/example/MyApplication.kt
@@ -1,11 +1,18 @@
 package com.example
 
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
 @RestController
 @SpringBootApplication
 class MyApplication {
 
   @RequestMapping("/")
-  fun home() = "Hello World!"
+  fun home(): String {
+    return "Hello World!"
+  }
 
 }
 
