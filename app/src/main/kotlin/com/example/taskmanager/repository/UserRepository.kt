378af0e356d7439a959ce9f5bab17153a74af9a4
WNomunomu <kohsei.nmr@gmail.com>
2025-08-11 04:21:31 +0000

    fix UserRepository

--- app/src/main/kotlin/com/example/taskmanager/repository/UserRepository.kt
+++ app/src/main/kotlin/com/example/taskmanager/repository/UserRepository.kt
@@ -7,8 +7,8 @@ import java.util.*
 
 @Repository
 interface UserRepository: JpaRepository<User, Long> {
-  fun findByUsername(username: string): Optional<User>
-  fun findByEmail(email: string): Optional<User>
-  fun existsByUsername(username: string): Boolean
-  fun existsByEmail(email: string): Boolean
+  fun findByUsername(username: String): Optional<User>
+  fun findByEmail(email: String): Optional<User>
+  fun existsByUsername(username: String): Boolean
+  fun existsByEmail(email: String): Boolean
 }
